af2a0196aa36d821773ee506e814472b8352b538
kolosovpetro <kolosovp94@gmail.com>
2024-06-29 14:53:53 +0200

    work in progress

--- mathematica/PolynomialIdentitiesInvolvingRascalTriangle.m
+++ mathematica/PolynomialIdentitiesInvolvingRascalTriangle.m
@@ -6,7 +6,8 @@ RascalNumber::usage="Gives generalized rascal triangle via sum of product \\rasc
 OneQBinomial::usage="Gives (1,q)-Binomial coefficient, see https://oeis.org/A096940"
 ColumnIdentity1::usage="Validates column identity \\rascalNumber{n}{k}{i} = \\binom{n}{k}, \\quad 0 \\leq k \\leq i"
 ColumnIdentity2::usage="Validates column identity \\rascalNumber{n}{i-j}{i}        &= \\binom{n}{i-j}"
-RowIdentity1::usage="Validates column identity \\rascalNumber{2i+1-j}{k}{i}             &= \\binom{2i+1-j}{k}"
+RowIdentity1::usage="Validates row identity \\rascalNumber{2i+1-j}{k}{i}             &= \\binom{2i+1-j}{k}"
+RowIdentity2::usage="Validates row identity \\rascalNumber{t-j}{k}{t-i-1}            &= \\binom{t-j}{k}"
 
 Begin["`Private`"]
 
@@ -24,6 +25,7 @@ OneQBinomial[n_, m_, q_] := OneQBinomial[n, m, q] = Which[
 ColumnIdentity1[rowsNumber_, i_]:= Column[Table[RascalNumber[n, i-j, i]== Binomial[n, i-j], {n, 0, rowsNumber}, {j, 0, i}]];
 ColumnIdentity2[rowsNumber_, i_]:= Column[Table[RascalNumber[n, n-i+j, i]== Binomial[n, n-i+j], {n, 0, rowsNumber}, {j, 0, i}]];
 RowIdentity1[i_]:= Column[Table[RascalNumber[2i+1-j, k, i]== Binomial[2i+1-j, k], {j, 0, 2i+1}, {k, 0, 2i+1-j}]];
+RowIdentity2[t_, i_]:= Column[Table[RascalNumber[t-j, k, t-i-1]== Binomial[t-j, k], {j, 0, t}, {k, 0, t-j}]];
 
 End[ ]
 

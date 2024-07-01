5d0281ea3f3a25b69930346a99826c3d2d9d52cb
kolosovpetro <kolosovp94@gmail.com>
2024-07-01 11:57:22 +0200

    work in progress

--- mathematica/IdentitiesInRascalTriangle.m
+++ mathematica/IdentitiesInRascalTriangle.m
@@ -7,6 +7,7 @@ OneQBinomial::usage="Gives (1,q)-Binomial coefficient, see https://oeis.org/A096
 ColumnIdentity1::usage="Validates column identity \\rascalNumber{n}{k}{i} = \\binom{n}{k}, \\quad 0 \\leq k \\leq i"
 ColumnIdentity2::usage="Validates column identity \\rascalNumber{n}{i-j}{i}        &= \\binom{n}{i-j}"
 RowIdentity1::usage="Validates row identity \\rascalNumber{2i+1-j}{k}{i}             &= \\binom{2i+1-j}{k}"
+PrintRowIdentity1::usage="Prints row identity \\rascalNumber{2i+1-j}{k}{i}             &= \\binom{2i+1-j}{k}"
 RowIdentity2::usage="Validates row identity \\rascalNumber{t-j}{k}{t-i-1}            &= \\binom{t-j}{k}"
 RowIdentity3::usage="Validates row identity \\rascalNumber{2i+1-j}{j}{i}         &= \\binom{2i+1-j}{j}"
 RowIdentity4::usage="Validates row identity \\rascalNumber{t-j}{t-2j}{t-i-1}     &= \\binom{t-j}{t-2j}, \\quad t \\geq 2i+1"
@@ -34,6 +35,7 @@ OneQBinomial[n_, m_, q_] := OneQBinomial[n, m, q] = Which[
 ColumnIdentity1[rowsNumber_, i_]:= Column[Table[RascalNumber[n, i-j, i]== Binomial[n, i-j], {n, 0, rowsNumber}, {j, 0, i}]];
 ColumnIdentity2[rowsNumber_, i_]:= Column[Table[RascalNumber[n, n-i+j, i]== Binomial[n, n-i+j], {n, 0, rowsNumber}, {j, 0, i}]];
 RowIdentity1[i_]:= Column[Table[RascalNumber[2i+1-j, k, i]== Binomial[2i+1-j, k], {j, 0, 2i+1}, {k, 0, 2i+1-j}]];
+PrintRowIdentity1[i_]:= Column[Table[RascalNumber[2i+1-j, k, i], {j, 0, 2i+1}, {k, 0, 2i+1-j}]];
 RowIdentity2[t_, i_]:= Column[Table[RascalNumber[t-j, k, t-i-1]== Binomial[t-j, k], {j, 0, t}, {k, 0, t-j}]];
 RowIdentity3[i_]:= Column[Table[RascalNumber[2i+1-j, j, i]== Binomial[2i+1-j, j], {j, 0, i}]];
 RowIdentity4[t_, i_]:= Column[Table[RascalNumber[t-j, t-2j, t-i-1]== Binomial[t-j, t-2j], {j, 0, t-i-1}]];
@@ -48,6 +50,9 @@ VandermondeIdentity[a_, b_, r_, upperLimit_] := Sum[Binomial[a, m] * Binomial[b,
 End[ ]
 
 EndPackage[ ]
+
+
+
 
 
 
